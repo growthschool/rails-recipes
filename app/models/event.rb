b99c96770ee3621cb079d93748d9959d48f5511f
Wen-Tien Chang <ihower@gmail.com>
2017-04-10 19:52:30 +0800

    Add Event#friendly_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,17 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name
+  validates_presence_of :name
+
+  before_validation :generate_friendly_id, :on => :create
+
+  def to_param
+    self.friendly_id
+  end
+
+  protected
+
+  def generate_friendly_id
+    self.friendly_id = SecureRandom.uuid
+  end
 
 end
