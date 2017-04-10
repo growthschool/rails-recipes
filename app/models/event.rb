49fffeedd5fa26c5c414945c8d8e3e4bcce6ab07
Wen-Tien Chang <ihower@gmail.com>
2017-04-10 20:05:33 +0800

    Admin can edit friendly_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
 
-  validates_presence_of :name
+  validates_presence_of :name, :friendly_id
+
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
 
   before_validation :generate_friendly_id, :on => :create
 
@@ -11,7 +14,7 @@ class Event < ApplicationRecord
   protected
 
   def generate_friendly_id
-    self.friendly_id = SecureRandom.uuid
+    self.friendly_id ||= SecureRandom.uuid
   end
 
 end
