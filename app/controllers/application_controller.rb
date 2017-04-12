01f1f32c727ac0720baa9cea37e1821e310369ac
Wen-Tien Chang <ihower@gmail.com>
2017-04-12 16:39:05 +0800

    Support timezone select

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
+  before_action :set_timezone
 
   def set_locale
     # 可以將 ["en", "zh-CN"] 設定為 VALID_LANG 放到 config/environment.rb 中
@@ -10,6 +11,12 @@ class ApplicationController < ActionController::Base
     end
 
     I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  def set_timezone
+    if current_user && current_user.time_zone
+      Time.zone = current_user.time_zone
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resource :user
+
   resources :events
 
   namespace :admin do
