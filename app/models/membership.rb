295f5b53aef085cf5fe7ece2171e04d368c116e9
Wen-Tien Chang <ihower@gmail.com>
2017-04-15 20:37:07 +0800

    Admin can edit multiple user groups

---
+++ app/models/membership.rb
@@ -0,0 +1,6 @@
+class Membership < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :group
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :memberships
+  has_many :groups, :through => :memberships
+
   def display_name
     self.email.split("@").first
   end
