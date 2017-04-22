711c129b0c9f5909ae12a83c4a2ef8a6309438b8
Wen-Tien Chang <ihower@gmail.com>
2017-04-22 23:34:51 +0800

    Add my profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,15 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
 
   def edit
-    @user = current_user
-    @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
-    @user.create_profile unless @user.profile
-
     if @user.update(user_params)
       flash[:notice] = "修改成功"
       redirect_to edit_user_path
@@ -20,6 +19,11 @@ class UsersController < ApplicationController
   end
 
   protected
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
+  end
 
   def user_params
     params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty] )
