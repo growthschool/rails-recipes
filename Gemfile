69d041fd506ea67ca8166e6a0a33355c33c6d7c4
Wen-Tien Chang <ihower@gmail.com>
2017-05-08 19:16:08 +0800

    Install letter_opener

--- Gemfile
+++ Gemfile
@@ -74,6 +74,7 @@ end
 
 group :development do
   gem 'faker'
+  gem 'letter_opener'
 
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.delivery_method = :letter_opener
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
 end
