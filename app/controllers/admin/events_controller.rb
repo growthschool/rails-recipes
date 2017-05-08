4172ea634651216cc4a49d683cac156aa6e747b8
Wen-Tien Chang <ihower@gmail.com>
2017-05-08 13:01:50 +0800

    refactors

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -7,13 +7,54 @@ class Admin::EventsController < AdminController
   def show
     @event = Event.find_by_friendly_id!(params[:id])
 
-    @label = @event.tickets.map { |t| t.name }
-    @data1 = @event.tickets.map{ |t| t.registrations.count }
-    @data2 = @event.tickets.map{ |t| t.registrations.count * t.price }
+    colors = [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ]
+
+    ticket_names = @event.tickets.map { |t| t.name }
+
+    @data1 = {
+        labels: ticket_names,
+        datasets: [{
+            label: '# of Registrations',
+            data: @event.tickets.map{ |t| t.registrations.count },
+            backgroundColor: colors,
+            borderWidth: 1
+        }]
+    }
+
+    @data2 = {
+        labels: ticket_names,
+        datasets: [{
+            label: '# of Amount',
+            data:  @event.tickets.map{ |t| t.registrations.by_status("confirmed").count * t.price },
+            backgroundColor: colors,
+            borderWidth: 1
+        }]
+    }
 
     if @event.registrations.any?
-      @dates = (@event.registrations.order("id ASC").first.created_at.to_date..Date.today).to_a
-      @data3 = @dates.map{ |d| @event.registrations.where( "created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count }
+      dates = (@event.registrations.order("id ASC").first.created_at.to_date..Date.today).to_a
+      colors = { "confirmed" => "#FF6384",
+                 "pending" => "#36A2EB"}
+
+      @data3 = {
+        labels: dates,
+        datasets: Registration::STATUS.map do |s|
+          {
+            :label => I18n.t(s, :scope => "registration.status"),
+            :data => dates.map{ |d|
+              @event.registrations.by_status(s.downcase).where( "created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count
+            },
+            borderColor: colors[s]
+          }
+        end
+      }
     end
 
   end
