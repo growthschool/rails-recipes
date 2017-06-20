1f97ee407e5063dff83300dd304c57c949fceacd
eagle1111 <wang499817054@gmail.com>
2017-06-20 21:53:17 +0800

    自定义Id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name
-
+ validates_presence_of :name, :friendly_id
+ validates_uniqueness_of :friendly_id
+ validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
  before_validation :generate_friendly_id, :on => :create
 
  def to_param
