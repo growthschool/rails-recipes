3029b8d13b7a6d10eb1a95a95b0c19a41da2f128
eagle1111 <wang499817054@gmail.com>
2017-06-20 21:39:25 +0800

    乱数

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
+    @event = Event.find_by_friendly_id!(params[:id])
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,15 @@ class Event < ApplicationRecord
 
  validates_presence_of :name
 
+ before_validation :generate_friendly_id, :on => :create
+
  def to_param
-   "#{self.id}-#{self.name}"
+   self.friendly_id
+ end
+
+ protected
+
+ def generate_friendly_id
+   self.friendly_id ||= SecureRandom.uuid
  end
 end
