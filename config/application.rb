8cf6fa3c369feef6b14ca83df33113f0df28fdc8
zhoujin <jin100chow@hotamil.com>
2017-07-10 19:24:34 +0800

    add t

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module RailsRecipes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = "zh-CN"
   end
 
-  config.i18n.default_locale = "zh-CN"
+
 end
