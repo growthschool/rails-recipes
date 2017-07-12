da348b6cf678d0bfa0e2b9ac49eb5f8bc8b8e334
zhoujin <jin100chow@hotamil.com>
2017-07-12 21:41:11 +0800

    add resources UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,14 @@ Rails.application.routes.draw do
   resource :user
   namespace :admin do
     root "events#index"
-    resources :events
+    resources :events do
+    resources :tickets, :controller => "event_tickets"
+    end
     resources :users do
       resource :profile, :controller => "user_profiles"
     end
   end
+
 
   root "events#index"
 
