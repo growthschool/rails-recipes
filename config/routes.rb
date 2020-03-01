27a0f1e9638f7de0d1edc558cbc60b09f867bec5
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-03-02 00:50:56 +0800

    edit profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
 
   resource :user
 
+
   namespace :admin do
     root "events#index"
     resources :events
-    resources :users
+    resources :users do
+      resource :profile, :controller => "user_profiles"
+    end
   end
 
   root "events#index"
