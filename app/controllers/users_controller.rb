cc33ed894715ccd733f152d52078b3cde069d709
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-03-03 00:21:03 +0800

    nested model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
+    @user.create_profile unless @user.profile
   end
 
   def update
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
   protected
 
   def user_params
-    params.require(:user).permit(:time_zone)
+    params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty])
   end
 
 end
