5a03bb100acf50c775f0ef9609da219112a50a6a
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-03-25 18:01:42 +0800

    split CSS and JS

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require select2
+//= require nested_form_fields
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
-//= require_tree .
