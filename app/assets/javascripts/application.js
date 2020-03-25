db1bf57a61deee97ff5001aa5151d90632cef7be
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-03-25 16:57:47 +0800

    change birthday select

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,6 @@
 //= require bootstrap-sprockets
 //= require select2
 //= require nested_form_fields
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require_tree .
