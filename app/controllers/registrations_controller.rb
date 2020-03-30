bf744d661b6d6662b41c4eb4451b9978a0a8b9c7
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-03-30 23:33:40 +0800

    add conditional validations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,6 +10,7 @@ class RegistrationsController < ApplicationController
     @registration.ticket = @event.tickets.find( params[:registration][:ticket_id] )
     @registration.status = "pending"
     @registration.user = current_user
+    @registration.current_step =1
 
     if @registration.save
       redirect_to step2_event_registration_path(@event, @registration)
@@ -28,6 +29,7 @@ class RegistrationsController < ApplicationController
 
   def step1_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 1
 
     if @registration.update(registration_params)
       redirect_to step2_event_registration_path(@event, @registration)
@@ -42,6 +44,7 @@ class RegistrationsController < ApplicationController
 
   def step2_update
     @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.current_step = 2
 
     if @registration.update(registration_params)
       redirect_to step3_event_registration_path(@event, @registration)
@@ -57,6 +60,7 @@ class RegistrationsController < ApplicationController
   def step3_update
     @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.status = "confirmed"
+    @registration.current_step = 3
 
     if @registration.update(registration_params)
       flash[:notice] = "报名成功"
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,9 @@
 class Registration < ApplicationRecord
 
+  attr_accessor :current_step
+  validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+  validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
+
   STATUS = ["pending", "confirmed"]
   validates_inclusion_of :status, :in => STATUS
   validates_presence_of :status, :ticket_id
@@ -15,6 +19,14 @@ class Registration < ApplicationRecord
   end
 
   protected
+
+  def should_validate_basic_data?
+    current_step == 2 # 只有做到第二步需要验证
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed" # 做到第三步，或最后状态是confirmed时需要验证
+  end
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
