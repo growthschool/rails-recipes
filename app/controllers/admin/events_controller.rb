6f8f1145fc8fa804453add066f3f81dcd489cf30
RealAnalysis <RealAnalysis@RealAnalysiss-iMac.local>
2020-04-10 23:06:45 +0800

    line chart

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -17,14 +17,19 @@ class Admin::EventsController < AdminController
 
      ticket_names = @event.tickets.map { |t| t.name }
 
+     status_colors = { "confirmed" => "#FF6384",
+                       "pending" => "#36A2EB" }
+
      @data1 = {
-          labels: ticket_names,
-          datasets: [{
-              label: "# of Registrations",
-              data: @event.tickets.map{ |t| t.registrations.count },
-              backgroundColor: colors,
-              borderWidth: 1
-            }]
+         labels: ticket_names,
+         datasets: Registration::STATUS.map do |s|
+           {
+             label: I18n.t(s, :scope => "registration.status"),
+             data: @event.tickets.map{ |t| t.registrations.by_status(s).count },
+             backgroundColor: status_colors[s],
+             borderWidth: 1
+           }
+         end
      }
 
      @data2 = {
@@ -36,6 +41,23 @@ class Admin::EventsController < AdminController
            borderWidth: 1
            }]
      }
+
+     if @event.registrations.any?
+       dates = (@event.registrations.order("id ASC").first.created_at.to_date..Date.today).to_a
+
+       @data3 = {
+         labels: dates,
+         datasets: Registration::STATUS.map do |s|
+           {
+             :label => I18n.t(s, :scope => "registration.status"),
+             :data => dates.map{ |d|
+               @event.registrations.by_status(s).where( "created_at >= ? AND created_at <= ?", d.beginning_of_day, d.end_of_day).count
+             },
+             borderColor: status_colors[s]
+           }
+         end
+       }
+     end
   end
 
   def new
