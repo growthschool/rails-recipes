366b5fb76835873ecf3eccef8ae729283fc1bde0
Gene <gorange310@gmail.com>
2021-02-26 19:45:34 +0800

    add friendly_id to events

---
+++ db/migrate/20210226113357_add_friendly_id_to_events.rb
@@ -0,0 +1,10 @@
+class AddFriendlyIdToEvents < ActiveRecord::Migration[5.0]
+  def change
+    add_column :events, :friendly_id, :string
+    add_index :events, :friendly_id, :unique => true
+
+    Event.find_each do |e|
+      e.update(:friendly_id => SecureRandom.uuid)
+    end
+  end
+end
