5fa46e5b9716b3e9e99adb28f491375852ae93c5
Gene <gorange310@gmail.com>
2021-02-27 13:23:26 +0800

    admin update friendly_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
 
-  validates_presence_of :name
+  validates_presence_of :name, :friendly_id
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  # 格式只限小寫英數字及橫線
 
   before_validation :generate_friendly_id, :on => :create
 
