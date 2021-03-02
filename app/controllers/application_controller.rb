839078cbe82408aa7c4b01e26465dd9c24fc9a7b
Gene <gorange310@gmail.com>
2021-03-02 16:21:32 +0800

    application_controller: before_action set_timezone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :set_timezone
+
   def set_locale
     # 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
     if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
@@ -8,5 +10,11 @@ class ApplicationController < ActionController::Base
     end
 
     I18n.locale = session[:locale] || I18n.default_locale
+  end
+
+  def set_timezone
+    if current_user && current_user.time_zone
+      Time.zone = current_user.time_zone
+    end
   end
 end
