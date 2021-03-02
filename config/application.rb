127cdb262986445996eadeb23973ed2f94545aff
Gene <gorange310@gmail.com>
2021-03-02 15:54:32 +0800

    add timezone: Taipei

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module RailsRecipes
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = "zh-TW"
+    config.time_zone = "Taipei"
   end
 end
