0064359f9b4e59584d14f449a203326bfb469a30
Gene <gorange310@gmail.com>
2021-03-03 16:59:43 +0800

    update Admin::UsersController

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,26 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render "edit"
+    end
+  end
+
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
 end
