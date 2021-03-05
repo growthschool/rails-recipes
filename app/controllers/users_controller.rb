8ca492617f17714f94acad8a4c58afcedec09b9e
Gene <gorange310@gmail.com>
2021-03-05 21:48:07 +0800

    add users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_user
+
+  def show
+  end
+
   def edit
-    @user = current_user
     # 跟剛才後台情況一樣，如果沒有 @user.profile，要先新建一個
     # unless @user.profile 等同於 if !@user.profile 或 if @user.profile.nil?
     @user.create_profile unless @user.profile
   end
 
   def update
-    @user = current_user
-
     if @user.update(user_params)
       flash[:notice] = "修改成功"
       redirect_to edit_user_path
@@ -21,5 +24,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:time_zone, :profile_attributes => [:id, :legal_name, :birthday, :location, :education, :occupation, :bio, :specialty])
+  end
+
+  def find_user
+    @user = current_user
+    @user.create_profile unless @user.profile
   end
 end
