d634a6c77c4587e2ef09a881bb0ab8d3f723714a
Gene <gorange310@gmail.com>
2021-03-06 14:49:31 +0800

    add event_ticket Index template

--- app/controllers/admin/event_tickets_controller.rb
+++ app/controllers/admin/event_tickets_controller.rb
@@ -1,2 +1,14 @@
 class Admin::EventTicketsController < ApplicationController
+  before_action :find_event
+
+  def index
+    @tickets = @event.tickets
+  end
+
+
+  protected
+
+  def find_event
+    @event = Event.find_by_friendly_id!(params[:event_id])
+  end
 end
