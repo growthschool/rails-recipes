f80dc01a267176d6557bb67075290cae256cb4eb
Gene <gorange310@gmail.com>
2021-03-08 17:59:36 +0800

    update schema: TABLE users add_column, add_index

--- db/migrate/20210308095450_add_row_order_to_users.rb
+++ db/migrate/20210308095450_add_row_order_to_users.rb
@@ -1,4 +1,11 @@
 class AddRowOrderToUsers < ActiveRecord::Migration[5.0]
   def change
+    add_column :users, :row_order, :integer
+
+    User.find_each do |u|
+      u.update( :row_order => :last )
+    end
+
+    add_index :users, :row_order
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210308082530) do
+ActiveRecord::Schema.define(version: 20210308095450) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -97,8 +97,10 @@ ActiveRecord::Schema.define(version: 20210308082530) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "time_zone"
+    t.integer  "row_order"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["row_order"], name: "index_users_on_row_order"
   end
 
 end
