b2fd2e55a1a299f9c6baddbf9d921b2b88e7d848
Gene <gorange310@gmail.com>
2021-03-08 21:42:15 +0800

    add registration "new" method: route, method, view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,5 @@
 class RegistrationsController < ApplicationController
+  def new
+    @event = Event.find_by_friendly_id(params[:event_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :events
+  resources :events do
+    resources :registrations
+  end
+
   resource :user
 
   namespace :admin do
