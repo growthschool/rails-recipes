8c8ec82832cdbd0e6385eb358d05c1725ddb5cbd
Gene <gorange310@gmail.com>
2021-03-13 16:10:20 +0800

    Update routes: steps2, steps2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :events do
-    resources :registrations
+    resources :registrations do
+      member do
+        get 'steps/2' => 'registrations#step2', :as => :step2
+        patch 'steps/2/update' => 'registrations#step2_update', :as => :update_step2
+        get 'steps/3' => 'registrations#step3', :as => :step3
+        patch 'steps/3/update' => 'registrations#step3_update', :as => :update_step3
+      end
+    end
   end
 
   resource :user
