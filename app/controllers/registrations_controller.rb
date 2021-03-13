85462bfad97ef7eb3e15f66492d704d2734974cd
Gene <gorange310@gmail.com>
2021-03-13 17:05:56 +0800

    Add registration step3

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -31,6 +31,22 @@ before_action :find_event
     end
   end
 
+  def step3
+    @registration = @event.registrations.find_by_uuid(params[:id])
+  end
+
+  def step3_update
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.status = "confirmed"
+
+    if @registration.update(registration_params)
+      flash[:notice] = "報名成功"
+      redirect_to event_registration_path(@event, @registration)
+    else
+      render "step3"
+    end
+  end
+
   def show
     @registration = @event.registrations.find_by_uuid(params[:id])
   end
