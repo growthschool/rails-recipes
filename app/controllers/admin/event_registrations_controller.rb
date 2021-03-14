62a9abd47b9e8678b5559e060671473206a78d17
Gene <gorange310@gmail.com>
2021-03-14 21:16:05 +0800

    Add Admin::event_registration destroy

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -5,11 +5,21 @@ class Admin::EventRegistrationsController < ApplicationController
     @registrations = @event.registrations.includes(:ticket).order("id DESC")
   end
 
+  def destroy
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    @registration.destroy
+
+    redirect_to admin_event_registrations_path(@event)
+  end
 
   protected
 
   def find_event
     @event = Event.find_by_friendly_id!(params[:event_id])
+  end
+
+  def registration_params
+    params.require(:registration).permit(:status, :ticket_id, :name, :email, :cellphone, :website, :bio)
   end
 
 end
