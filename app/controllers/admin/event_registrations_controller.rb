431f816e9a253da04ae751b8cc9ad468cb7d13ef
Gene <gorange310@gmail.com>
2021-03-24 00:23:53 +0800

    Add event_registrations export CVS

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Admin::EventRegistrationsController < ApplicationController
   before_action :find_event
 
@@ -24,6 +25,19 @@ class Admin::EventRegistrationsController < ApplicationController
       @registrations = @registrations.where( :id => params[:registration_id].split(",") )
     end
 
+    respond_to do |format|
+      format.html
+      format.csv {
+        @registrations = @registrations.reorder("id ASC")
+        csv_string = CSV.generate do |csv|
+          csv << ["報名ID", "票種", "姓名", "狀態", "Email", "報名時間"]
+          @registrations.each do |r|
+            csv << [r.id, r.ticket.name, r.name, t(r.status, :scope => "registration.status"), r.email, r.created_at]
+          end
+        end
+        send_data csv_string, :filename => "#{@event.friendly_id}-registrations-#{Time.now.to_s(:number)}.csv"
+      }
+    end
   end
 
   def destroy
